6afa42d3cd185d486361e484bc5f68aa3a18aa57
Danil <totem7h@gmail.com>
2021-10-11 12:27:28 +0300

    4.3.5. Множественное логическое выражение И [#9461 #214635]

--- src/main/java/condition/Triangle.java
+++ src/main/java/condition/Triangle.java
@@ -2,10 +2,6 @@ package condition;
 
 public class Triangle {
 
-    public static void main(String[] args) {
-
-    }
-
     public static boolean exist(double ab, double ac, double bc) {
         return ab + ac > bc && ac + bc > ab && ab + bc > ac;
     }
