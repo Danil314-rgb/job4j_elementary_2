9bf4693f6ee63ce2102f27a64e42edb1dd2bf45e
Danil <totem7h@gmail.com>
2021-10-11 11:37:41 +0300

    4.3.3. Логическое отрицание ! [#334072 #214629]

--- src/main/java/condition/LogicNot.java
+++ src/main/java/condition/LogicNot.java
@@ -5,27 +5,27 @@ public class LogicNot {
     public static void main(String[] args) {
     }
 
-    public static boolean isEven(int num) { // число чётное
+    public static boolean isEven(int num) {
         return num % 2 == 0;
     }
 
-    public static boolean isPositive(int num) { // число положительное
+    public static boolean isPositive(int num) {
         return num > 0;
     }
 
-    public static boolean notEven(int num) { // число не чётное
+    public static boolean notEven(int num) {
         return !isEven(num);
     }
 
-    public static boolean notPositive(int num) { // число не положительное
+    public static boolean notPositive(int num) {
         return !isPositive(num);
     }
 
-    public static boolean notEvenAndPositive(int num) { // число нечетное и положительное;
+    public static boolean notEvenAndPositive(int num) {
         return notEven(num) && isPositive(num);
     }
 
-    public static boolean evenOrNotPositive(int num) { // число четное или не положительное.
+    public static boolean evenOrNotPositive(int num) {
         return isEven(num) || notPositive(num);
     }
 }
