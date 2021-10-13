2d24b7f9cc51b89bf2b86ef95030021a52b93bdb
Danil <totem7h@gmail.com>
2021-10-13 10:40:26 +0300

    6.0.1.1. Размер массива [#333582 #215047]

--- src/main/java/array/ArrayDefinition.java
+++ src/main/java/array/ArrayDefinition.java
@@ -3,15 +3,12 @@ package array;
 public class ArrayDefinition {
 
     public static void main(String[] args) {
-        String[] names = new String[4];
-        names[0] = "Petr";
-        names[1] = "Ivan";
-        names[2] = "Oleg";
-        names[3] = "Bob";
+        short[] ages = new short[10];
+        String[] surname = new String[100500];
+        float[] prices = new float[40];
 
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        System.out.println(ages.length);
+        System.out.println(surname.length);
+        System.out.println(prices.length);
     }
 }
