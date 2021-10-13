ef1e13829fc6aa3c891025fff9f11f47ee5d81ee
Danil <totem7h@gmail.com>
2021-10-13 14:32:21 +0300

    6.0.3. Массивы и цикл for. [#226665 #215054]

--- src/main/java/array/ArrayDefinition.java
+++ src/main/java/array/ArrayDefinition.java
@@ -4,10 +4,10 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         int[] arr = new int[5];
-        for (int i = 0; i <= arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             arr[i] = i * 2 + 3;
         }
-        for (int i = 0; i <= arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             System.out.println(arr[i]);
         }
     }
