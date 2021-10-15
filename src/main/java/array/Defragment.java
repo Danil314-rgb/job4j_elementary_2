cd1e7bb2d6e82e161a9b65c87eb3adcb0e1f2362
Danil <totem7h@gmail.com>
2021-10-15 10:35:49 +0400

    6.8. Дефрагментация массива [#225 #216025]

--- src/main/java/array/Defragment.java
+++ src/main/java/array/Defragment.java
@@ -14,23 +14,15 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int nPoint = index;
-                for (int i = index; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        int point = i;
-                        swap(array, point, nPoint);
+                        array[index] = array[i];
+                        array[i] = null;
                         break;
                     }
                 }
             }
         }
-        return array;
-    }
-
-    public static String[] swap(String[] array, int source, int dest) {
-        String temp = array[source];
-        array[source] = array[dest];
-        array[dest] = temp;
         return array;
     }
 }
