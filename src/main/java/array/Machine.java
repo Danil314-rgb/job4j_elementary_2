0895df61893bb86ac46271cbe763672a0892930e
Danil <totem7h@gmail.com>
2021-10-16 12:43:53 +0400

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #216069]

--- src/main/java/array/Machine.java
+++ src/main/java/array/Machine.java
@@ -6,17 +6,15 @@ public class Machine {
 
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
-        int indexC = 0;
         int size = 0;
         int[] result = new int[100];
         money = money - price;
-        while (money > 0) {
-            while (money >= coins[indexC]) {
-                money = money - coins[indexC];
-                result[size] = coins[indexC];
+        for (int indexCoins : coins) {
+            while (money >= indexCoins) {
+                money = money - indexCoins;
+                result[size] = indexCoins;
                 size++;
             }
-            indexC++;
         }
         return Arrays.copyOf(result, size);
     }
