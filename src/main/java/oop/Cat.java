f55a40c97306dfb1fc772f80f4b2114d04eec65c
Danil <totem7h@gmail.com>
2021-10-18 10:28:13 +0400

    1.5. Поля объекта [#166083 #217080]

--- src/main/java/oop/Cat.java
+++ src/main/java/oop/Cat.java
@@ -2,19 +2,6 @@ package oop;
 
 public class Cat {
 
-    public static void main(String[] args) {
-        System.out.println("There are gav's food.");
-        Cat gav = new Cat();
-        gav.giveNick("gav");
-        gav.eat("kotleta");
-        gav.show();
-        System.out.println("There are black's food.");
-        Cat black = new Cat();
-        black.giveNick("black");
-        black.eat("fish");
-        black.show();
-    }
-
     private String food;
     private String name;
 
@@ -28,5 +15,18 @@ public class Cat {
 
     public void giveNick(String nick) {
         this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("fish");
+        black.show();
     }
 }
