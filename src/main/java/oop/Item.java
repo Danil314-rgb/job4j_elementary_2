3b3a82c3b013433880e5937d48bc30073953f6b8
Danil <totem7h@gmail.com>
2021-10-18 16:21:28 +0400

    2. Перегрузка конструктора [#173890]

---
+++ src/main/java/oop/Item.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Item {
+
+    private int id;
+    private String name;
+
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
