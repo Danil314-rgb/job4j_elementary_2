68451af5e222aa8cad434e81dfd79add7418d501
Danil <totem7h@gmail.com>
2021-10-19 16:32:11 +0400

    4. Переопределение [#174890 #217599]

--- src/main/java/oop/JSONReport.java
+++ src/main/java/oop/JSONReport.java
@@ -7,7 +7,7 @@ public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
         String srt = "\"";
-        return "{" + System.lineSeparator() + " \"name\" : " + "\"" + name + "\""
+        return "{" + System.lineSeparator() + " \"name\" : " + "\"" + name + "\"" + ","
                 + System.lineSeparator() + " \"body\" : " + "\"" + body + "\"" + System.lineSeparator() + "}";
     }
 }
