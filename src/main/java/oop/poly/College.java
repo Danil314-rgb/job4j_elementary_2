46f122ac78dd522f260cff96b73029390a601639
Danil <totem7h@gmail.com>
2021-10-21 13:03:41 +0400

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/oop/poly/College.java
@@ -0,0 +1,10 @@
+package oop.poly;
+
+public class College {
+
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student student = freshman;
+        Object obj = student;
+    }
+}
---
+++ src/main/java/oop/poly/Freshman.java
@@ -0,0 +1,4 @@
+package oop.poly;
+
+public class Freshman extends Student {
+}
