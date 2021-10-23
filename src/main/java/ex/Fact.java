51446b25c022c42da549c269ff848aba77343960
Danil <totem7h@gmail.com>
2021-10-23 16:09:35 +0400

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #219603]

--- src/main/java/ex/Fact.java
+++ src/main/java/ex/Fact.java
@@ -4,7 +4,7 @@ public class Fact {
 
     public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("n should be larger zero");
+            throw new IllegalArgumentException("n should be larger or equal to zero");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
