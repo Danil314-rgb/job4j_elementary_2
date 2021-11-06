bbc0fa16cebc563c43aad58e3063546f19baa128
Danil <totem7h@gmail.com>
2021-11-06 12:04:34 +0400

    2. Идентичные тексты [#212655]

---
+++ src/main/java/set/UniqueText.java
@@ -0,0 +1,22 @@
+package set;
+
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        HashSet<String> check = new HashSet<>();
+        String[] origin = originText.split(" ");
+        for (String item : origin) {
+            check.add(item);
+        }
+        String[] text = duplicateText.split(" ");
+        for (String item : text) {
+            if (!check.contains(item)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
