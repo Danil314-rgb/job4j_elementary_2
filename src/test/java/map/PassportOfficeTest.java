6e0e05111d26f5fc487f939276e8c3d0484ef4e1
Danil <totem7h@gmail.com>
2021-11-06 17:47:31 +0400

    1. Паспорт и жители [#212656]

---
+++ src/test/java/map/PassportOfficeTest.java
@@ -0,0 +1,17 @@
+package map;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("298444", "Danil");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+}
