ef93871d63c5ccbaeafae929d2d5ebc948f45df7
Danil <totem7h@gmail.com>
2021-11-07 17:18:02 +0400

    1. Паспорт и жители [#212656 #225370]

--- src/test/java/map/PassportOfficeTest.java
+++ src/test/java/map/PassportOfficeTest.java
@@ -8,10 +8,20 @@ import static org.junit.Assert.*;
 public class PassportOfficeTest {
 
     @Test
-    public void add() {
+    public void whenAdd() {
         Citizen citizen = new Citizen("298444", "Danil");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenNotAdd() {
+        Citizen citizen = (new Citizen("123123", "Tom"));
+        Citizen citizen2 = (new Citizen("123123", "Tom"));
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen2);
+        assertFalse(office.add(citizen2));
     }
 }
