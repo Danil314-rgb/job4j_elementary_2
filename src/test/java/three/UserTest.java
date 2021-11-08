4c61845477fdde32c240bbe8778ff74231c8af89
Danil <totem7h@gmail.com>
2021-11-08 14:45:43 +0400

    1. Организовать сортировку User [#10034]

---
+++ src/test/java/three/UserTest.java
@@ -0,0 +1,40 @@
+package three;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void compareTo() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Petr", 32)));
+    }
+
+    @Test
+    public void whenCompareTwoIvan() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Ivan", 32));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Ivan", 32)));
+    }
+
+    @Test
+    public void whenCompare() {
+        int res = new User("Petr", 32).compareTo(new User("Ivan", 31));
+        assertThat(res, greaterThan(0));
+    }
+}
