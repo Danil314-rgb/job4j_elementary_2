b6b74a76a1470e34ee0cee633ebef35afcd3bac4
Danil <totem7h@gmail.com>
2021-11-09 11:38:31 +0400

    -

--- src/test/java/three/StringCompareTest.java
+++ src/test/java/three/StringCompareTest.java
@@ -30,26 +30,6 @@ public class StringCompareTest {
     }
 
     @Test
-    public void whenLeftGreaterThanRightResultShouldBePositive() {
-        StringCompare compare = new StringCompare();
-        int rst = compare.compare(
-                "Petrov",
-                "Ivanova"
-        );
-        assertThat(rst, greaterThan(0));
-    }
-
-    @Test
-    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
-        StringCompare compare = new StringCompare();
-        int rst = compare.compare(
-                "Petrov",
-                "Patrov"
-        );
-        assertThat(rst, greaterThan(0));
-    }
-
-    @Test
     public void secondCharOfLeftLessThanRightShouldBeNegative() {
         StringCompare compare = new StringCompare();
         int rst = compare.compare(
