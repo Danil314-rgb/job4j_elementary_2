51680a12acf0813b444723f706ededa6380af5ae
Danil <totem7h@gmail.com>
2021-11-09 12:14:03 +0400

    3. Компаратор для строк [#35008 #226241]

--- src/main/java/three/StringCompare.java
+++ src/main/java/three/StringCompare.java
@@ -6,16 +6,13 @@ public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-
-        int result = -1;
-        if (left.length() == right.length()) {
-            for (int i = 0; i < left.length(); i++) {
-                int res = Character.compare(left.charAt(i), right.charAt(i));
-                if (res == 0) {
-                    result = 0;
-                }
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            int result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
+                return result;
             }
         }
-        return result;
+        return Integer.compare(left.length(), right.length());
+
     }
 }
