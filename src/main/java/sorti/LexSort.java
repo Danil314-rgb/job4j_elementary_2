a19f0a9f97cc491bc31caf5cb54facbec3dd6fbc
Danil <totem7h@gmail.com>
2021-11-11 11:42:19 +0400

    4. Сортировка номера [#221284 #226510]

--- src/main/java/sorti/LexSort.java
+++ src/main/java/sorti/LexSort.java
@@ -8,15 +8,11 @@ public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftStr = left.split("\\.");
         String[] rightStr = right.split("\\.");
-
-        for (int i = 0; i < 1; i++) {
-            int a = Integer.parseInt(leftStr[i]);
-            int b = Integer.parseInt(rightStr[i]);
-            if (Integer.compare(a, b) != 0) {
-                return Integer.compare(a, b);
-            }
+        int a = Integer.parseInt(leftStr[0]);
+        int b = Integer.parseInt(rightStr[0]);
+        if (Integer.compare(a, b) != 0) {
+            return Integer.compare(a, b);
         }
-
         return left.compareTo(right);
     }
 }
