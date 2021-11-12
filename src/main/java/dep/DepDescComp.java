7c5b9e6d07a372e75a61ff5769f753e5e0c74d87
Danil <totem7h@gmail.com>
2021-11-12 11:00:58 +0400

    2. Отсортировать департаменты [#34352 #227344]

--- src/main/java/dep/DepDescComp.java
+++ src/main/java/dep/DepDescComp.java
@@ -6,18 +6,9 @@ public class DepDescComp implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        int res = 0;
         String[] oo1 = o1.split("/");
         String[] oo2 = o2.split("/");
-        for (int i = 0; i < Math.min(oo1.length, oo2.length); i++) {
-            res = oo2[i].compareTo(oo1[i]);
-            if (res == 0) {
-                res = oo1[i].compareTo(oo2[i]);
-            }
-        }
-        if (res == 0 && oo1.length != oo2.length) {
-            res = Integer.compare(oo1.length, oo2.length);
-        }
-        return res;
+        int res = oo2[0].compareTo(oo1[0]);
+        return res == 0 ? o1.compareTo(o2) : res;
     }
 }
--- src/main/java/dep/Departments.java
+++ src/main/java/dep/Departments.java
@@ -9,13 +9,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (start.equals("")) {
-                    tmp.add(el);
-                    start = start + el;
-                } else {
-                    tmp.add(start +  "/" + el);
-                    start = start + "/" + el;
-                }
+                start += "".equals(start) ? el : "/" + el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
